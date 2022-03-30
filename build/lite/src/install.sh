70c74f4a649e66232316dfe8cf56b07a2d9e968e
Arno05 <47012355+Arno05@users.noreply.github.com>
2022-03-30 10:21:50 +0200

    Update install.sh

--- build/lite/src/install.sh
+++ build/lite/src/install.sh
@@ -15,5 +15,5 @@ chmod a+wr /etc/earnapp/
 touch /etc/earnapp/status
 chmod a+wr /etc/earnapp/status
 printf $EARNAPP_UUID > /etc/earnapp/uuid
-earnapp --verbose start
-earnapp --verbose run
+earnapp start
+earnapp run
